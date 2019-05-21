c7ae518167889f8f9f963b85ab016ae79ed355ee
Lorenzo Cattarino <lcattari@dide.local>
2019-05-22 00:27:43 +0100

    change file paths

--- data-raw/create_data.R
+++ data-raw/create_data.R
@@ -1,12 +1,13 @@
 # create data
 
-trait_data <- read.csv("aegyptiDENVmodelTempData_2016-03-30.csv",
+trait_data <- read.csv(file.path("data-raw",
+                                 "aegyptiDENVmodelTempData_2016-03-30.csv"),
                        header = TRUE)
 
-aedes_aegypti_priors <- read.csv("Aedes_prior_data.csv",
+aedes_aegypti_priors <- read.csv(file.path("data-raw", "Aedes_prior_data.csv"),
                                  header = TRUE)
 
-aa_EIP_priors <- read.csv("EIP_priors_2015-12-04.csv",
+aa_EIP_priors <- read.csv(file.path("data-raw", "EIP_priors_2015-12-04.csv"),
                           header = TRUE)
 
 usethis::use_data(trait_data,
