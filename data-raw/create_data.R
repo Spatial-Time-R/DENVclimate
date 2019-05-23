9a5c0253c65778d20b9f05a9db467fed3831e05c
Lorenzo Cattarino <lcattari@imperial.ac.uk>
2019-05-23 14:07:30 -0700

    create more data

--- data-raw/create_data.R
+++ data-raw/create_data.R
@@ -10,7 +10,17 @@ aedes_aegypti_priors <- read.csv(file.path("data-raw", "Aedes_prior_data.csv"),
 aa_EIP_priors <- read.csv(file.path("data-raw", "EIP_priors_2015-12-04.csv"),
                           header = TRUE)
 
+foi_data <- read.csv(file.path("data-raw",
+                               "All_FOI_estimates_and_predictors.csv"),
+                     stringsAsFactors = FALSE)
+
+predictor_rank <- read.csv(file.path("data-raw",
+                                     "predictor_rank.csv"),
+                           stringsAsFactors = FALSE)
+
 usethis::use_data(trait_data,
                   aedes_aegypti_priors,
                   aa_EIP_priors,
+                  foi_data,
+                  predictor_rank,
                   overwrite = TRUE)
