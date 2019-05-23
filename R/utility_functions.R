3d68deaeb41925b54be5712a447a7ef6361178f3
Lorenzo Cattarino <lcattari@imperial.ac.uk>
2019-05-23 11:35:11 -0700

    add save_plot

--- R/utility_functions.R
+++ R/utility_functions.R
@@ -1,3 +1,9 @@
+# -----------------------------------------------------------------------------
+#
+# Utility functions
+#
+#------------------------------------------------------------------------------
+
 #------------------------------------------------------------------------------
 #' loop
 #'
@@ -97,4 +103,33 @@ lm_eqn <- function(df, y, x){
                    list(b = format(coef(m), digits = 4),
                         r2 = format(r2, digits = 4)))
   as.character(as.expression(eq))
+}
+
+
+#------------------------------------------------
+#' save_plot
+#'
+#' \code{save_plot} save a png file of a plot
+#'
+#' @param plot_obj The plot object
+#' @param out_pth The path where to save the plot
+#' @param out_fl_nm The output file name
+#' @param wdt The plot width
+#' @param hgt The plot height
+#'
+#' @export
+
+
+save_plot <- function(plot_obj, out_pth, out_fl_nm, wdt, hgt){
+
+  dir.create(out_pth, FALSE, TRUE)
+  png(file.path(out_pth, paste0(out_fl_nm, ".png")),
+      width = wdt,
+      height = hgt,
+      units = "cm",
+      pointsize = 12,
+      res = 300)
+  print(plot_obj)
+  on.exit(dev.off())
+
 }
